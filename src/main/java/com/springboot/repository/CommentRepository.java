a62758997a86c5b78db9cf32093673610c7b1461
Jagruti Goswami <jagrutigoswami402@gmail.com>
2023-08-15 23:36:04 +0530

    created Comment Repository interface

---
+++ src/main/java/com/springboot/repository/CommentRepository.java
@@ -0,0 +1,8 @@
+package com.springboot.repository;
+
+import com.springboot.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
