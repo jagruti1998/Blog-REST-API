d892a76ff04c681193a44dd0d8620876df9f2b7f
Jagruti Goswami <jagrutigoswami402@gmail.com>
2023-08-16 18:43:56 +0530

    commit

--- src/main/java/com/springboot/dtos/CommentDto.java
+++ src/main/java/com/springboot/dtos/CommentDto.java
@@ -3,11 +3,21 @@ package com.springboot.dtos;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Data
 public class CommentDto {
     private long id;
 
+    //name should not be null or empty
+    @NotEmpty
     private String name;
+    @NotEmpty
+    @Email
     private String email;
+    @NotEmpty
+    @Size(min=10,message = "Comment body should have 10 chars")
     private String body;
 }
