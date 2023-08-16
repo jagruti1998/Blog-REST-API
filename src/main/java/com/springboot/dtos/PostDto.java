c74d0a54a6df7c155e4d9657ed341d1d455ba1b9
Jagruti Goswami <jagrutigoswami402@gmail.com>
2023-08-16 15:21:56 +0530

    Custom Exception Handling

--- src/main/java/com/springboot/dtos/PostDto.java
+++ src/main/java/com/springboot/dtos/PostDto.java
@@ -6,12 +6,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
+import java.util.Set;
+
 @Data //lombok annotations
 public class PostDto {
-
-
     private Long id;
     private String title;
     private String description;
     private String content;
+    private Set<CommentDto> comments;
 }
