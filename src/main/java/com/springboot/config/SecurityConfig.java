2237a163d5d974ef68259da186a5749cdb5bf16c
Jagruti Goswami <jagrutigoswami402@gmail.com>
2023-08-17 23:59:42 +0530

    logindto class

--- src/main/java/com/springboot/config/SecurityConfig.java
+++ src/main/java/com/springboot/config/SecurityConfig.java
@@ -3,9 +3,12 @@ package com.springboot.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -17,6 +20,17 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    private UserDetailsService userDetailsService;
+    public SecurityConfig(UserDetailsService userDetailsService){
+        this.userDetailsService=userDetailsService;
+
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
+    }
 
     @Bean
     public static PasswordEncoder passwordEncoder(){
@@ -37,7 +51,7 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
+ /*   @Bean
     public UserDetailsService userDetailsService(){
         UserDetails jaggu = User.builder()
                                 .username("jaggu")
@@ -53,4 +67,6 @@ public class SecurityConfig {
 
         return new InMemoryUserDetailsManager(jaggu,admin);
     }
+
+  */
 }
---
+++ src/main/java/com/springboot/dtos/LoginDto.java
@@ -0,0 +1,17 @@
+package com.springboot.dtos;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginDto {
+    
+    private String usernameOrEmail;
+    private String password;
+}
