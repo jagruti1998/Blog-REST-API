0a838df3befc1b0dc999d3f2d4ea7520af4fce08
Jagruti Goswami <jagrutigoswami402@gmail.com>
2023-08-17 23:21:28 +0530

    established manyto many relationship between user and role jpa entities

--- src/main/java/com/springboot/entity/User.java
+++ src/main/java/com/springboot/entity/User.java
@@ -4,6 +4,8 @@ package com.springboot.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.Set;
+
 @Setter
 @Getter
 @NoArgsConstructor
@@ -22,4 +24,10 @@ public class User {
     private String email;
     @Column(nullable = false)
     private String password;
+
+
+    @ManyToMany(fetch = FetchType.EAGER,cascade = CascadeType.ALL)//whenever we load user entity along with that it will load its roles
+    @JoinTable(name = "users_roles",joinColumns = @JoinColumn(name = "user_id",referencedColumnName = "id"),
+    inverseJoinColumns=@JoinColumn(name = "role_id",referencedColumnName = "id"))
+    private Set<Role> roles;
 }
