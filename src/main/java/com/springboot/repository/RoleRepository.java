d81f76a52bb4d12c3054411e8d6eb0150d21be4e
Jagruti Goswami <jagrutigoswami402@gmail.com>
2023-08-17 23:28:18 +0530

    role repository created

---
+++ src/main/java/com/springboot/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.springboot.repository;
+
+import com.springboot.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role,Long> {
+
+    Optional<Role> findByIdName(String name);
+}
