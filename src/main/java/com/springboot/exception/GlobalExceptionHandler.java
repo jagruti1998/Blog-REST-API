e0595999e393d4fcf979986667cc950cf9708817
Jagruti Goswami <jagrutigoswami402@gmail.com>
2023-08-17 23:05:08 +0530

    added AccessDeniedException

--- src/main/java/com/springboot/exception/GlobalExceptionHandler.java
+++ src/main/java/com/springboot/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ package com.springboot.exception;
 import com.springboot.dtos.ErrorDetails;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -37,6 +38,15 @@ public class GlobalExceptionHandler {
 
         ErrorDetails errorDetails=new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+
+    }
+
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorDetails> handleAccessDeniedException(AccessDeniedException exception, WebRequest webRequest){
+
+        ErrorDetails errorDetails=new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.UNAUTHORIZED);
 
     }
 
