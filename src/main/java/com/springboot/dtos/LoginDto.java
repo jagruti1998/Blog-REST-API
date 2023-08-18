ec784a2b697d9c7a531606358d25e0a2b38be7cb
Jagruti Goswami <jagrutigoswami402@gmail.com>
2023-08-18 11:19:20 +0530

    build login rest api

--- src/main/java/com/springboot/dtos/LoginDto.java
+++ src/main/java/com/springboot/dtos/LoginDto.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginDto {
-    
+
     private String usernameOrEmail;
     private String password;
 }
---
+++ src/main/java/com/springboot/service/AuthService.java
@@ -0,0 +1,8 @@
+package com.springboot.service;
+
+import com.springboot.dtos.LoginDto;
+
+public interface AuthService {
+
+    String login(LoginDto loginDto);
+}
