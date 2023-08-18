43af62198711d9d09af7aa347d1cbcf2ffa35f2b
Jagruti Goswami <jagrutigoswami402@gmail.com>
2023-08-18 12:58:53 +0530

    Customizing Swagger Models documentation

--- src/main/java/com/springboot/dtos/PostDto.java
+++ src/main/java/com/springboot/dtos/PostDto.java
@@ -1,5 +1,6 @@
 package com.springboot.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 
 import javax.validation.constraints.NotEmpty;
@@ -10,10 +11,13 @@ import lombok.Data;
 import java.util.Set;
 
 @Data //lombok annotations
+@Schema(
+        description = "PostDto Model Information"
+)
 public class PostDto {
     private Long id;
 
-
+    @Schema(description = "Blog Post title")
     //title should not be null or empty
     //title should have atleast two characters
 
@@ -21,12 +25,18 @@ public class PostDto {
     @Size(min=2,message = "POst title should have atleast 2 chars")
     private String title;
 
+
+
+    @Schema(description = "Blog Post description")
     //title should not be null or empty
     //title should have atleast 10 characters
     @NotEmpty
     @Size(min=10,message = "Post description should have 10 chars")
     private String description;
 
+
+
+    @Schema(description = "Blog Post content")
     //title should not be null or empty
     @NotEmpty
     private String content;
